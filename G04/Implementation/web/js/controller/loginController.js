f19fe68f18f8ee41cda06107ab7159b2e9512d90
JohnJay <xiangjiechou@gmail.com>
2016-03-08 19:57:48 +0800

    change variable name

--- G04/Implementation/web/js/controller/loginController.js
+++ G04/Implementation/web/js/controller/loginController.js
@@ -1,19 +1,16 @@
-
-$(document).ready(function(){
-	$("button").click(function(){
-		alert();
-	   var name = $("input#userId").val();
-	   var password = $("input#password").val();
-	   $.ajax({
-		   type:'POST',
-		   url:'api/adminLogin.php',
-		   data:{username:name,password:password},
-		   success:function(data){
-			   alert(data);
-		   },
-		   error:function(status){
-			   alert(JSON.stringify(status))
-		   }
-	   })
-	});
+$("button#login").click(function() {
+  var username = $("input#userId").val();
+  var password = $("input#password").val();
+  $.post("api/adminLogin.php", {
+      username: username,
+      password: password
+    },
+    function(data, status) {
+      alert(data);
+      if (data == 1) {
+        window.location.assign("index.html");
+      } else {
+        alert("用户名或密码不正确");
+      }
+    });
 });
