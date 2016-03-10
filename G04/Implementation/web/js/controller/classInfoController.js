a0ed2eeaafa72e73ceeb56afc2552fcf4648bd34
JohnJay <xiangjiechou@gmail.com>
2016-03-10 15:42:43 +0800

    interactive with json

--- G04/Implementation/web/js/controller/classInfoController.js
+++ G04/Implementation/web/js/controller/classInfoController.js
@@ -1,2 +1,3 @@
- $(".print-body").load("classInfo.html  .table");
+  $("section").append(window.localStorage.html);
+ //$(".print-body").load("classInfo.html  .table");
  setTimeout("window.print()",100); 
