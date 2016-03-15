eb1a4c2c707a947ca3e88c9379369bd6e17f5178
Tony.Zhu <863716623@qq.com>
2016-03-15 13:40:39 +0800

    excel操作类，信息导入api,Android端登录api

---
+++ G04/Implementation/web/api/app/config.php
@@ -0,0 +1,7 @@
+﻿<?php
+	define('DB_HOST','56d6b10631055.sh.cdb.myqcloud.com');
+	define('DB_USER','cdb_outerroot');
+	define('DB_PSW','metis123456');
+	define('DB_NAME','metis');
+	define('DB_PORT','4003');
+?>
---
+++ G04/Implementation/web/api/web/regInfoImport.php
@@ -0,0 +1,22 @@
+<?php
+include_once("./Excel/reader.php");
+	$filename=$_FILES["excel"]["name"];
+	$dir=$filename;
+	if(!move_uploaded_file($_FILES["excel"]["tmp_name"],$dir)){
+		$logger->warn("upload failed.");
+		$logger->warn("excel save failed: $dir");
+		$logger->warn("upload tmp_name is: " . $_FILES["photo"]["tmp_name"]);
+		$logger->warn("upload name is: " . $_FILES["photo"]["name"]);
+		echo "upload error";
+		return;
+	}
+
+	//循环读取上传的表格数据并插入数据库
+	$data = new Spreadsheet_Excel_Reader();
+	$data->setOutputEncoding('utf-8');
+	$data->read($dir);
+	for ($i = 1; $i <= $data->sheets[0]['numRows']; $i++) {
+		for ($j = 1; $j <= $data->sheets[0]['numCols']; $j++) {
+			echo $param[$j]=$data->sheets[0]['cells'][$i][$j];
+		}
+	}
