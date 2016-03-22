cb410671becd28f1fe1be86acf6de72f435a4654
JohnJay <xiangjiechou@gmail.com>
2016-03-22 13:01:45 +0800

    根据接口进行修改

--- G04/Implementation/web/js/controller/classInfoController.js
+++ G04/Implementation/web/js/controller/classInfoController.js
@@ -1,3 +1,34 @@
-  $("section").append(window.localStorage.html);
- //$(".print-body").load("classInfo.html  .table");
- setTimeout("window.print()",100); 
+// if (window.localStorage) {
+// 	alert("浏览支持localStorage")
+// } else {
+// 	alert("浏览暂不支持localStorage")
+// }
+
+$.get("api/web/getClassInfo.php", function(data) {
+	var jsonArray = eval(data);
+	var item;
+	for (item in jsonArray) {
+		//将科目加入下拉列表中
+		$(".dropdown-menu").append("<li><a href='#'>" + jsonArray[item].subject_name + "</a></li>");
+		$("tbody").append("<tr><td>" + jsonArray[item].class_no + "</td><td>" + jsonArray[item].subject_name + "</td><td>" + jsonArray[item].Date + "</td><td>" + jsonArray[item].expert_name + "</td><td>" + jsonArray[item].expert_mobilephone + "</td></tr>");
+	}
+	$(".dropdown-menu").append("<li class='divider'></li>");
+	$(".dropdown-menu").append("<li><a href='#'>" + "所有" + "</a></li>");
+
+	window.localStorage.html = $("#table").html();
+
+	$(document).ready(function() {
+		$(".btn-group .dropdown-menu li a").click(function() {
+			$("tbody").empty();
+			for (item in jsonArray) {
+				if($(this).html() == jsonArray[item].subject_name || $(this).html() == "所有")
+				$("tbody").append("<tr><td>" + jsonArray[item].class_no + "</td><td>" + jsonArray[item].subject_name + "</td><td>" + jsonArray[item].Date + "</td><td>" + jsonArray[item].expert_name + "</td><td>" + jsonArray[item].expert_mobilephone + "</td></tr>");
+			}
+			window.localStorage.html = $("#table").html();
+		});
+
+
+	});
+
+});
+
