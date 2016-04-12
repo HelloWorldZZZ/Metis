9eced4fd39b6fdfef42f1275b26b19eb5dfbecc7
JohnJay <xiangjiechou@gmail.com>
2016-04-12 15:23:22 +0800

    button

--- G04/Implementation/web/js/controller/classInfoController.js
+++ G04/Implementation/web/js/controller/classInfoController.js
@@ -9,16 +9,16 @@ $.get("api/web/getClassInfo.php", function(data) {
 	var item;
 	for (item in jsonArray) {
 		//将科目加入下拉列表中
-		$(".dropdown-menu").append("<li><a href='#'>" + jsonArray[item].subject_name + "</a></li>");
+		$("#teacherDrop").append("<li><a href='#'>" + jsonArray[item].subject_name + "</a></li>");
 		$("tbody").append("<tr><td>" + jsonArray[item].class_no + "</td><td>" + jsonArray[item].subject_name + "</td><td>" + jsonArray[item].Date + "</td><td>" + jsonArray[item].expert_name + "</td><td>" + jsonArray[item].expert_mobilephone + "</td></tr>");
 	}
-	$(".dropdown-menu").append("<li class='divider'></li>");
-	$(".dropdown-menu").append("<li><a href='#'>" + "所有" + "</a></li>");
+	$("#teacherDrop").append("<li class='divider'></li>");
+	$("#teacherDrop").append("<li><a href='#'>" + "所有" + "</a></li>");
 
 	window.localStorage.html = $("#table").html();
 
 	$(document).ready(function() {
-		$(".btn-group .dropdown-menu li a").click(function() {
+		$(".btn-group #teacherDrop li a").click(function() {
 			$("tbody").empty();
 			for (item in jsonArray) {
 				if($(this).html() == jsonArray[item].subject_name || $(this).html() == "所有")
--- G04/Implementation/web/js/controller/studentInfoController.js
+++ G04/Implementation/web/js/controller/studentInfoController.js
@@ -1,78 +1,83 @@
 $.get("api/web/getStudentList.php", function(data) {
-  processData(data, true);
-  //动态生成下拉框
-  $(".dropdown-menu").append("<li class='divider'></li>");
-  $(".dropdown-menu").append("<li><a href='#'>" + "所有" + "</a></li>");
+
+	processData(data, true);
+	//动态生成下拉框
 });
 
 
 
 $(".btn-success").click(function() {
-  $.post("api/web/getStudentList.php", {
-      task: "distribute"
-    },
-    function(data) {
-      processData(data, false);
-    });
+	$.post("api/web/getStudentList.php", {
+			task: "distribute"
+		},
+		function(data) {
+			processData(data, false);
+		});
 });
 
 
 function processData(data, flag) {
-  var jsonArray = eval(data);
-  var item;
-  var t = $('table').dataTable();
-  t.fnClearTable();
+	var jsonArray = eval(data);
+	var item;
+	var t = $('table').dataTable();
+	t.fnClearTable();
 
-  for (item in jsonArray) {
+	for (item in jsonArray) {
 
-    if ($("#selector").html() == (jsonArray[item].class_no + "考场") || $("#selector").html() == "选择考场") {
-      var student_list = jsonArray[item].student_list;
-      var in_item;
-      if (flag)
-        $(".dropdown-menu").append("<li><a href='#'>" + jsonArray[item].class_no + "考场" + "</a></li>");
+		if ($("#selector").html() == (jsonArray[item].class_no + "考场") || $("#selector").html() == "选择考场") {
+			var student_list = jsonArray[item].student_list;
+			var in_item;
+			if (flag)
+				$("#classDrop").append("<li><a href='#'>" + jsonArray[item].class_no + "考场" + "</a></li>");
 
-      for (in_item in student_list) {
-        t.fnAddData([
-          "大连市第二十四中学",
-          jsonArray[item].class_no,
-          jsonArray[item].sub_type_name,
-          jsonArray[item].subject_name,
-          student_list[in_item].student_name,
-          student_list[in_item].test_num,
-          student_list[in_item].test_temp_id
-        ]);
-      }
-    }
-  }
-
-  window.localStorage.html = $(".table").html();
+			for (in_item in student_list) {
+				t.fnAddData([
+					"大连市第二十四中学",
+					jsonArray[item].class_no,
+					jsonArray[item].sub_type_name,
+					jsonArray[item].subject_name,
+					student_list[in_item].student_name,
+					student_list[in_item].test_num,
+					student_list[in_item].test_temp_id
+				]);
+			}
 
 
-  $(document).ready(function() {
-    $(".dropdown-menu li a").click(function() {
-      $("#selector").html($(this).html());
-      t.fnClearTable();
-      for (item in jsonArray) {
-        if ($(this).html() == (jsonArray[item].class_no + "考场") || $(this).html() == "所有") {
-          var student_list = jsonArray[item].student_list;
-          var in_item;
-          for (in_item in student_list) {
-            t.fnAddData([
-              "大连市第二十四中学",
-              jsonArray[item].class_no,
-              jsonArray[item].sub_type_name,
-              jsonArray[item].subject_name,
-              student_list[in_item].student_name,
-              student_list[in_item].test_num,
-              student_list[in_item].test_temp_id
-            ]);
-          }
-        }
-      }
-      window.localStorage.html = $(".table").html();
+		}
+	}
 
-    });
-  });
+	if (flag) {
+		$("#classDrop").append("<li class='divider'></li>");
+		$("#classDrop").append("<li><a href='#'>" + "所有" + "</a></li>");
+	}
+	
+	window.localStorage.html = $(".table").html();
+
+	$(document).ready(function() {
+		$("#classDrop li a").click(function() {
+			$("#selector").html($(this).html());
+			t.fnClearTable();
+			for (item in jsonArray) {
+				if ($(this).html() == (jsonArray[item].class_no + "考场") || $(this).html() == "所有") {
+					var student_list = jsonArray[item].student_list;
+					var in_item;
+					for (in_item in student_list) {
+						t.fnAddData([
+							"大连市第二十四中学",
+							jsonArray[item].class_no,
+							jsonArray[item].sub_type_name,
+							jsonArray[item].subject_name,
+							student_list[in_item].student_name,
+							student_list[in_item].test_num,
+							student_list[in_item].test_temp_id
+						]);
+					}
+				}
+			}
+			window.localStorage.html = $(".table").html();
+
+		});
+	});
 
 
 }
